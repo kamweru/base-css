e03e364aa37597c7445dd417f86c578e0b35e5cd
kamweru <subtillia@gmail.com>
2024-04-22 23:34:54 +0300

    button config

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,8 +1,18 @@
 <script>
+  let btnHeight = 32;
 </script>
 
 <div class="flex h:100%">
   <div>
-    <button>button</button>
+    <button class="$btn-height:{btnHeight}px btn" disabled
+      >default button</button
+    >
+    <button class="btn btn-fill">fill button</button>
+    <button class="btn btn-dashed">dashed button</button>
+    <button class="btn btn-text">text button</button>
+    <button class="btn btn-link">link button</button>
+  </div>
+  <div>
+    <input type="number" name="" id="" bind:value={btnHeight} />
   </div>
 </div>
