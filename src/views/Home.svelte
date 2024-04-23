b8dac433bdfef44b3114cc3a2b7fc579212a33f3
kamweru <subtillia@gmail.com>
2024-04-24 01:02:15 +0300

    fix: success button

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -2,7 +2,7 @@
   let btnHeight = 32;
 </script>
 
-<div class="flex h:100%">
+<div class="h:100% flex flex:col gap:16">
   <div>
     <button class="$btn-height:{btnHeight}px btn" disabled
       >default button</button
@@ -11,6 +11,15 @@
     <button class="btn btn-dashed">dashed button</button>
     <button class="btn btn-text">text button</button>
     <button class="btn btn-link">link button</button>
+  </div>
+  <div>
+    <button class="$btn-height:{btnHeight}px btn btn-success" disabled
+      >default button</button
+    >
+    <button class="btn btn-fill btn-success">fill button</button>
+    <button class="btn btn-dashed btn-success">dashed button</button>
+    <button class="btn btn-text btn-success">text button</button>
+    <button class="btn btn-link btn-success">link button</button>
   </div>
   <div>
     <input type="number" name="" id="" bind:value={btnHeight} />
