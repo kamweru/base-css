7e639843eff92559218cca8ebc541a05dab18825
kamweru <subtillia@gmail.com>
2024-06-20 14:04:58 +0300

    add card and tag

--- src/lib/components/card/Card.svelte
+++ src/lib/components/card/Card.svelte
@@ -1,8 +1,54 @@
 <script>
   import { appStore } from "../../AppStore";
-
+  import "../styles/card.css";
   export let currentComponent;
 </script>
 
-<progress value="0.5" class={$appStore.properties[currentComponent].classes}
-></progress>
+<div class="flex flex:col gap:8">
+  <div class={$appStore.properties[currentComponent].classes}>
+    <div class="card-head">
+      <div class="card-head-content">
+        <div class="card-title">Card Title</div>
+        <div class="card-head-extra">more</div>
+      </div>
+    </div>
+    <div class="card-body">
+      {#each Array(4) as _}
+        <div class="p:">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      {/each}
+    </div>
+    <div class="card-footer">
+      <div class="card-footer-content">
+        <button class="btn btn-outline"> cancel </button>
+        <button class="btn btn-fill"> action </button>
+      </div>
+    </div>
+  </div>
+
+  <div class={$appStore.properties[currentComponent].classes}>
+    <div class="card-body">
+      {#each Array(4) as _}
+        <div class="p:">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      {/each}
+    </div>
+  </div>
+  <div class={$appStore.properties[currentComponent].classes}>
+    <div class="card-head">
+      <div class="card-head-content">
+        <div class="card-title">Card Title</div>
+        <div class="card-head-extra">more</div>
+      </div>
+    </div>
+    <div class="card-body">
+      {#each Array(4) as _}
+        <div class="p:">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
--- src/lib/components/tag/Tag.svelte
+++ src/lib/components/tag/Tag.svelte
@@ -1,8 +1,28 @@
 <script>
+  import Icon from "@iconify/svelte";
   import { appStore } from "../../AppStore";
-
+  import "../styles/tag.css";
   export let currentComponent;
 </script>
 
-<progress value="0.5" class={$appStore.properties[currentComponent].classes}
-></progress>
+<div class="">
+  <span class={$appStore.properties[currentComponent].classes}>Tag 1</span>
+  <span class={$appStore.properties[currentComponent].classes}
+    >Tag 1
+    <button class="tag-close-btn">
+      <span class="icon tag-close-icon">
+        <Icon icon="tabler:x" />
+      </span>
+    </button>
+  </span>
+
+  <span class={$appStore.properties[currentComponent].classes}>
+    <span class="icon">
+      <Icon icon="tabler:plus" />
+    </span>
+    <span>Tag 1</span>
+    <!-- <span class="icon tag-close-icon">
+      <Icon icon="tabler:x" />
+    </span> -->
+  </span>
+</div>
